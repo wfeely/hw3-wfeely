1677d693c94e6e1e05bf273fe045219ab901428d
wfeely <wfeely@cs.cmu.edu>
2013-09-30 20:40:27 -0400

    removed unnecessary imports from EvaluatorCasConsumer

--- hw3-wfeely/src/main/java/edu/cmu/deiis/casconsumers/EvaluatorCasConsumer.java
+++ hw3-wfeely/src/main/java/edu/cmu/deiis/casconsumers/EvaluatorCasConsumer.java
@@ -5,26 +5,16 @@
 package edu.cmu.deiis.casconsumers;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.cas.FSIndex;
-import org.apache.uima.cas.FSIterator;
-import org.apache.uima.cas.impl.XmiCasSerializer;
 import org.apache.uima.collection.CasConsumer_ImplBase;
-import org.apache.uima.examples.SourceDocumentInformation;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.ResourceProcessException;
-import org.apache.uima.util.XMLSerializer;
-import org.xml.sax.SAXException;
 
 import edu.cmu.deiis.types.AnswerScore;
 import edu.cmu.deiis.types.Question;
